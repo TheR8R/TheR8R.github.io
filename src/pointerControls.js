cfa9be5656ce9d0449ef42a8914bbe3f1fde7acd
TheR8R <202105925@post.au.dk>
2024-04-06 12:00:37 +0200

    initial commit

---
+++ src/pointerControls.js
@@ -0,0 +1,39 @@
+
+import { PointerLockControls } from './../node_modules/three/examples/jsm/controls/PointerLockControls.js';
+import  {paused} from './soundImpl.js';
+
+let first = false;
+export function pointerControls(camera) {
+    let controls = new PointerLockControls(camera, document.body );
+    
+    controls.maxPolarAngle = Math.PI / 2; // this locks the camera to the horizontal plane
+    controls.minPolarAngle = Math.PI / 2; // this locks the camera to the horizontal plane
+
+    const blocker = document.getElementById( 'blocker' );
+    const instructions = document.getElementById( 'instructions' );
+
+    instructions.addEventListener( 'click', function () {
+
+        controls.lock();
+
+    } );
+
+    controls.addEventListener( 'lock', function () {
+
+        instructions.style.display = 'none';
+        blocker.style.display = 'none';
+        if(first === false) {
+            first = true;
+        } else {
+            paused(1);
+        }
+    } );    
+    
+    controls.addEventListener( 'unlock', function () {  
+        
+        blocker.style.display = 'block';    
+        instructions.style.display = '';
+            paused(2);
+    } );
+    return controls;    
+}
---
+++ src/raycasters.js
@@ -0,0 +1,76 @@
+import {Vector3, Raycaster} from './../node_modules/three/src/Three.js';
+
+let arrowHelpers, raycasters, raycasterToRoof, arrowHelperToRoof, soundRaycasters, leftDistance, rightDistance, frontDistance, backDistance; 
+
+const directions = [
+        new Vector3(-1, 0, 0), // left
+        new Vector3(1, 0, 0), // right
+        new Vector3(0, 0, -1), // front
+        new Vector3(0, 0, 1), // back
+    ];
+
+
+export class createRaycasters {
+    constructor(camera, scene) {
+        this.camera = camera;
+        this.scene = scene;
+        this.init(); 
+    }
+
+    init() {
+        raycasters = [];
+        // arrowHelpers = [];
+        soundRaycasters = [];
+
+        for(let i = 0; i < directions.length; i++){
+            let raycaster = new Raycaster();
+            raycaster.set(this.camera.position, directions[i]);
+            raycasters.push(raycaster);
+    
+            // let arrowHelper = new THREE.ArrowHelper(directions[i], this.camera.position, 7, 0xFF0000);
+            // arrowHelpers.push(arrowHelper);
+        }
+        // for sound
+        for(let i = 0; i < 3; i++){
+            let raycaster = new Raycaster();
+            raycaster.set(this.camera.position, this.camera.getWorldDirection);
+            soundRaycasters.push(raycaster);
+        }
+        
+    }
+    getSoundRaycasters() {
+        return soundRaycasters;
+    }
+
+    getRaycasters() {
+        return raycasters;
+    }
+
+    getArrowHelpers() {
+        return arrowHelpers;
+    }
+
+    getRaycasterToRoof() {
+        return raycasterToRoof;
+    }
+
+    getArrowHelperToRoof() {
+        return arrowHelperToRoof;
+    }
+
+    getLeftAndRightDistance() {
+        for(let i = 0; i < soundRaycasters.length; i++){
+            let intersects = soundRaycasters[i].intersectObjects(this.scene.children);
+            if (intersects.length > 0 ) {
+                if (i === 0) {
+                    leftDistance = intersects[0].distance;
+                } else if (i === 1){
+                    rightDistance = intersects[0].distance;
+                } else {
+                    frontDistance = intersects[0].distance;
+                }
+            }
+        }
+        return [leftDistance, rightDistance, frontDistance];
+    }
+}
