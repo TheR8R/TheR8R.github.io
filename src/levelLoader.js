b8d30204acaf4a53aa4b8919500c34d01e698852
TheR8R <202105925@post.au.dk>
2024-04-06 11:27:42 +0200

    trying this2

--- src/levelLoader.js
+++ src/levelLoader.js
@@ -1,5 +1,5 @@
 import * as THREE from './../node_modules/three/src/Three.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GLTFLoader } from './../node_modules/three/examples/jsm/loaders/GLTFLoader.js';	
 import { nextLevelAudio, youFinishedAudio } from './soundImpl.js';
 
 let currentLevel;
