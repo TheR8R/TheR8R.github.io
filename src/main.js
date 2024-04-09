ac2b15753c688b79393f6891982a8a98e3bf3c67
TheR8R <202105925@post.au.dk>
2024-04-09 16:11:37 +0200

    now it works, and time to iron out bugs...

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
 
+import { movement} from './keyboardControls.js';
 import { loadLevel } from './levelLoader.js';
 import { playerClass } from './player.js';
-import { checkForCollision } from './Collision.js';
-import { movement} from './keyboardControls.js';
+import { checkForCollision } from './collision.js';
 import  {paused} from './soundImpl.js';
 
 let camera, scene, renderer, controls, raycasters, player;
